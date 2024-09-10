b8a80281917cfb3f6faad6bb70b0f23add655dff
AlvesLuis0 <alves.luis.3002@gmail.com>
2024-09-10 19:54:11 -0300

    feat: adicionar funções básicas no PersonDao

--- app/src/main/java/com/paydev/pagou/daos/PersonDao.kt
+++ app/src/main/java/com/paydev/pagou/daos/PersonDao.kt
@@ -1,8 +1,19 @@
 package com.paydev.pagou.daos
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Update
+import com.paydev.pagou.models.Person
 
 @Dao
 interface PersonDao {
-  //
+  @Insert
+  fun insert(person: Person): Long
+
+  @Update
+  fun update(person: Person): Int
+
+  @Delete
+  fun delete(person: Person): Int
 }
