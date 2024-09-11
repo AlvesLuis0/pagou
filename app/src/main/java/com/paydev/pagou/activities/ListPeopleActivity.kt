62c87c97508d8d2855cfc0b21220eb62db4381ef
AlvesLuis0 <alves.luis.3002@gmail.com>
2024-09-11 18:41:40 -0300

    style: reorganizar pastas

---
+++ app/src/main/java/com/paydev/pagou/activities/ListPeopleActivity.kt
@@ -0,0 +1,12 @@
+package com.paydev.pagou.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.paydev.pagou.R
+
+class ListPeopleActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_list_people)
+  }
+}
