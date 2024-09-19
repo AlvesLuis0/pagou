02363c1afa7c0ab8d56a4c155969c0bae1eb46c3
Will <williamcavalcante912@gmail.com>
2024-09-19 20:55:38 -0300

    feat: adicionado layout de transação
    O layout de transação foi adicionado junto com um botão provisório na tela principal para apresentar a nova interface.

---
+++ app/src/main/java/com/paydev/pagou/activities/ListTransactionsActivity.kt
@@ -0,0 +1,15 @@
+package com.paydev.pagou.activities
+
+internal class {
+    inner class ListTransactionsActivity
+
+    fun AppCompatActivity() {
+        var `fun`: override
+        onCreate(savedInstanceState)
+        if (Bundle)
+            run {
+                super.onCreate(savedInstanceState)
+                setContentView(R.layout.activity_list_people)
+            }
+    }
+}
