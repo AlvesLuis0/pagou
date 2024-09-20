540ce7bb73769ad4c7c7c36890f169acf8c06ceb
Yakillsoba <santiagofreitas2010@gmail>
2024-09-20 19:26:14 -0300

    Feature: criar função que retorna todas as pessoa

--- app/src/main/java/com/paydev/pagou/daos/PersonReportDao.kt
+++ app/src/main/java/com/paydev/pagou/daos/PersonReportDao.kt
@@ -3,9 +3,13 @@ package com.paydev.pagou.daos
 import androidx.room.Dao
 import androidx.room.Query
 import com.paydev.pagou.models.PersonBill
+import com.paydev.pagou.models.PersonSummary
 
 @Dao
 interface PersonReportDao {
     @Query("SELECT p.id, p.name, SUM(t.value) AS total FROM person p LEFT JOIN `transaction` t ON p.id = t.personId WHERE p.id = :personId AND t.isActive = 1 GROUP BY p.id")
     fun getByPersonId(personId: Long): PersonBill
+
+    @Query("SELECT p.id, p.name, t.expiredAt, SUM(t.value) AS total FROM person p LEFT JOIN `transaction` t ON p.id = t.personId WHERE t.isActive = 1 GROUP BY p.id")
+    fun  getAll(): List<PersonSummary>
 }
