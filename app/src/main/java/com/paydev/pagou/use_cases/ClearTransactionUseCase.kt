ffbe7e9cfb4bbf669ae134eada648577c6bd15cd
AlvesLuis0 <alves.luis.3002@gmail.com>
2024-09-21 14:54:02 -0300

    fix: arrumar função que retorna dados da transação pelo ID

--- app/src/main/java/com/paydev/pagou/use_cases/ClearTransactionUseCase.kt
+++ app/src/main/java/com/paydev/pagou/use_cases/ClearTransactionUseCase.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import com.paydev.pagou.services.DatabaseService
 
 class ClearTransactionUseCase(context: Context) {
+    private val _context = context
     private val transactionDao = DatabaseService.getInstance(context).transactionDao()
 
     fun execute(personId: Long){
+        (_context)
         transactionDao.deleteAllByPersonId(personId)
     }
 }
