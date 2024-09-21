820542acd15d9c2a15a7be1960f244b0c187c648
Gled-mdk <gleddragneel@gmail.com>
2024-09-21 15:44:23 -0300

    feat: adicionar a funcionalidade de obter relatório da pessoa

---
+++ app/src/main/java/com/paydev/pagou/use_cases/GetPersonReportUseCase.kt
@@ -0,0 +1,26 @@
+package com.paydev.pagou.use_cases
+
+import android.content.Context
+import com.paydev.pagou.models.PersonBill
+import com.paydev.pagou.models.TransactionSummary
+import com.paydev.pagou.services.DatabaseService
+
+class GetPersonReportUseCase (context: Context) {
+    private val personReportDao = DatabaseService
+        .getInstance(context)
+        .personReportDao()
+    private val transactionDao = DatabaseService
+        .getInstance(context)
+        .transactionDao()
+    data class PersonReport(
+        val personBill: PersonBill,
+        val transactions: List<TransactionSummary>
+    )
+
+    fun execute(id: Long): PersonReport {
+        val personBill = personReportDao.getByPersonId(id)
+        val transactions = transactionDao.getByPersonId(id)
+        return PersonReport(personBill, transactions)
+    }
+
+}
