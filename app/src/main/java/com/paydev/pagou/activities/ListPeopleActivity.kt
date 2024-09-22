1cca284cd1a7a5983ff12079b9ac715b658fe6bc
AlvesLuis0 <alves.luis.3002@gmail.com>
2024-09-22 13:35:06 -0300

    refactor: criar classe responsável por gerar dialog da pessoa

--- app/src/main/java/com/paydev/pagou/activities/ListPeopleActivity.kt
+++ app/src/main/java/com/paydev/pagou/activities/ListPeopleActivity.kt
@@ -1,19 +1,10 @@
 package com.paydev.pagou.activities
 
-import android.content.Intent
-import android.content.res.ColorStateList
-import android.graphics.Color
-import android.graphics.drawable.GradientDrawable
-import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.paydev.pagou.R
-import com.paydev.pagou.use_cases.CreatePersonUseCase
+import com.paydev.pagou.dialogs.PersonDialog
 
 class ListPeopleActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,49 +12,7 @@ class ListPeopleActivity : AppCompatActivity() {
     setContentView(R.layout.activity_list_people)
   }
 
-  fun onClick(view: View?) {
-    val builder = AlertDialog.Builder(this)
-    val view = layoutInflater.inflate(R.layout.activity_add_person, null)
-    val nameInput = view.findViewById<EditText>(R.id.name_input)
-    val contactInput = view.findViewById<EditText>(R.id.contact_input)
-    val othersInput = view.findViewById<EditText>(R.id.others_input)
-
-    builder.setView(view)
-      .setPositiveButton("OK") { _, _ ->
-        val name = nameInput.text.toString()
-        val contact = contactInput.text.toString()
-        val others = othersInput.text.toString()
-
-        val person = CreatePersonUseCase(this).execute(name, contact, others)
-
-        val intent = Intent(this, ListTransactionsActivity::class.java)
-        startActivity(intent)
-        // Faça algo com o texto do input
-      }
-      .setNegativeButton("Cancelar") { dialog, _ ->
-        dialog.cancel()
-      }
-      .setPositiveButton("Aceitar"){ dialog, which ->
-        Toast.makeText(this, "Pessoa adicioanda com sucesso", Toast.LENGTH_SHORT).show()
-
-      }
-
-    val alertDialog = builder.create()
-    alertDialog.show()
-
-    val positiveButton = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)
-
-    val colorStateList = ColorStateList(
-      arrayOf(
-        intArrayOf(android.R.attr.state_pressed),
-        intArrayOf()
-      ),
-      intArrayOf(
-        Color.parseColor("#BDE3D1"),
-        Color.parseColor("#BDE3D1")
-      )
-    )
-
-    positiveButton.setBackgroundTintList(colorStateList)
+  fun openAddPersonDialog(view: View?) {
+    PersonDialog(this).show()
   }
 }
---
+++ app/src/main/java/com/paydev/pagou/dialogs/PersonDialog.kt
@@ -0,0 +1,71 @@
+package com.paydev.pagou.dialogs
+
+import android.content.Context
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.paydev.pagou.R
+import com.paydev.pagou.activities.ListTransactionsActivity
+import com.paydev.pagou.use_cases.CreatePersonUseCase
+
+class PersonDialog(private val context: Context) {
+  // construindo dialog
+  private val dialogView = LayoutInflater
+    .from(context)
+    .inflate(R.layout.activity_add_person, null)
+  private val dialog = AlertDialog
+    .Builder(context)
+    .setView(dialogView)
+    .setNegativeButton("Cancelar") { dialog , _ -> dialog.cancel() }
+    .setPositiveButton("OK") { _, _ -> execute() }
+    .create()
+
+  // inputs
+  private val nameInput = dialogView
+    .findViewById<EditText>(R.id.name_input)
+  private val contactInput = dialogView
+    .findViewById<EditText>(R.id.contact_input)
+  private val othersInput = dialogView
+    .findViewById<EditText>(R.id.others_input)
+
+  // chama função ao apertar em OK
+  private fun execute() {
+    // criando pessoa
+    CreatePersonUseCase(context)
+      .execute(
+        nameInput.text.toString(),
+        contactInput.text.toString(),
+        othersInput.text.toString()
+      )
+    // mostra mensagem de sucesso
+    Toast
+      .makeText(context, "Pessoa adicionada com sucesso", Toast.LENGTH_SHORT)
+      .show()
+    // redireciona para próxima tela
+    val intent = Intent(context, ListTransactionsActivity::class.java)
+    context.startActivity(intent)
+  }
+
+  companion object fun show() {
+    // mostrando dialog
+    dialog.show()
+    // inserindo cor do botão
+    val colorStateList = ColorStateList(
+      arrayOf(
+        intArrayOf(android.R.attr.state_pressed),
+        intArrayOf()
+      ),
+      intArrayOf(
+        Color.parseColor("#BDE3D1"),
+        Color.parseColor("#BDE3D1")
+      )
+    )
+    dialog
+      .getButton(AlertDialog.BUTTON_POSITIVE)
+      .setBackgroundTintList(colorStateList)
+  }
+}
