9985513d09d254b208c3bcc6ea171e1baef77e0c
Yakillsoba <santiagofreitas2010@gmail>
2024-09-24 20:09:14 -0300

    Feature: Adicionar função de obter saldo do usuario

--- app/src/main/java/com/paydev/pagou/daos/PersonReportDao.kt
+++ app/src/main/java/com/paydev/pagou/daos/PersonReportDao.kt
@@ -7,9 +7,12 @@ import com.paydev.pagou.models.PersonReport
 
 @Dao
 interface PersonReportDao {
-    @Query("SELECT id,name,total FROM personreport WHERE id = :personId ")
+    @Query("SELECT id,name,total FROM personreport WHERE id = :personId")
     fun getByPersonId(personId: Long): PersonBill
 
     @Query("SELECT * FROM PersonReport")
-    fun  getAll(): List<PersonReport>
+    fun getAll(): List<PersonReport>
+
+    @Query("SELECT CASE WHEN :isPositive = 1 THEN TOTAL(CASE WHEN total > 0 THEN total ELSE 0 END) WHEN :isPositive = 0 THEN -TOTAL(CASE WHEN total < 0 THEN total ELSE 0 END) ELSE 0  END AS result FROM PersonReport;")
+    fun getTotal(isPositive: Boolean): Float
 }
