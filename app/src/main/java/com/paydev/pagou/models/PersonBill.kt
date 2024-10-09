4ec2c2ccb8b0104f09d26a17c0a4347f9a1bb202
AlvesLuis0 <alves.luis.3002@gmail.com>
2024-10-09 19:05:13 -0300

    feat: adicionar campo de contato no PersonReport

--- app/src/main/java/com/paydev/pagou/models/PersonBill.kt
+++ app/src/main/java/com/paydev/pagou/models/PersonBill.kt
@@ -5,6 +5,7 @@ import java.util.Date
 data class PersonBill(
     var id: Long,
     var name: String,
+    var contact: String,
     var lastPayment: Date?,
     var total: Float,
 )
--- app/src/main/java/com/paydev/pagou/models/PersonReport.kt
+++ app/src/main/java/com/paydev/pagou/models/PersonReport.kt
@@ -3,10 +3,11 @@ package com.paydev.pagou.models
 import androidx.room.DatabaseView
 import java.util.Date
 
-@DatabaseView ("SELECT p.id, p.name, MAX((SELECT t.registeredAt FROM `transaction` t WHERE t.personId = p.id AND t.isActive = 1)) AS lastPayment, TOTAL((SELECT t.value FROM `transaction` t WHERE t.personId = p.id AND t.isActive = 1)) AS total FROM person p LEFT JOIN `transaction` t ON p.id = t.personId GROUP BY p.id")
+@DatabaseView ("SELECT p.id, p.name, p.contact, MAX((SELECT t.registeredAt FROM `transaction` t WHERE t.personId = p.id AND t.isActive = 1)) AS lastPayment, TOTAL((SELECT t.value FROM `transaction` t WHERE t.personId = p.id AND t.isActive = 1)) AS total FROM person p LEFT JOIN `transaction` t ON p.id = t.personId GROUP BY p.id")
 data class PersonReport(
     var id: Long,
     var name: String,
+    var contact: String,
     var lastPayment: Date?,
     var total: Float,
 )
