156599befbc4f14df4664aaf8163b5a7aacb006c
AlvesLuis0 <alves.luis.3002@gmail.com>
2024-10-10 21:12:59 -0300

    fix: corrigir validação do tamanho das strings na entidade pessoa

--- app/src/main/java/com/paydev/pagou/models/Person.kt
+++ app/src/main/java/com/paydev/pagou/models/Person.kt
@@ -20,10 +20,10 @@ data class Person(
     if (contact.isBlank()){
       errors.add("número de contato não pode ser nulo")
     }
-    if(name.length in 2..17){
+    if(name.length !in 3..16){
       errors.add("O nome precisa ter entre 3 e 16 caracteres ")
     }
-    if(contact.length in 7..10){
+    if(contact.length !in 8..9){
       errors.add("O número de contato inválido")
     }
   }
