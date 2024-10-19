48de6b4daa82101ed777ad84e9b0a1812c6e33c4
AlvesLuis0 <alves.luis.3002@gmail.com>
2024-10-19 17:41:11 -0300

    fix: erros das cores ao selecionar o tema escuro no sistema do celular

--- app/src/main/java/com/paydev/pagou/activities/ListPeopleActivity.kt
+++ app/src/main/java/com/paydev/pagou/activities/ListPeopleActivity.kt
@@ -3,6 +3,7 @@ package com.paydev.pagou.activities
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.paydev.pagou.R
@@ -17,6 +18,7 @@ class ListPeopleActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
 
     super.onCreate(savedInstanceState)
+    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
     setContentView(R.layout.activity_list_people)
 
     // this creates a vertical layout Manager
